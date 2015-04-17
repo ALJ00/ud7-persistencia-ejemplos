1b1cd95c695ac330d23739f9c9f17a7f8abb4f24
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2015-04-17 20:08:46 +0200

    Eliminados imports superfluos

--- FicherosJava/src/ficherosjava/EjemploHTTP.java
+++ FicherosJava/src/ficherosjava/EjemploHTTP.java
@@ -11,8 +11,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class EjemploHTTP {
 
