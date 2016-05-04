23fd0536912167c1fedf5f984c27e7aa6040f21e
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2016-05-04 21:52:47 +0200

    Comentado

--- PruebaMySQL/src/pruebamysql/PruebaMySQL.java
+++ PruebaMySQL/src/pruebamysql/PruebaMySQL.java
@@ -11,23 +11,32 @@ public class PruebaMySQL {
     public static void main(String[] args) {
 
         try {
+
+            // [1] Conexión
+            
+            // Driver
             Class.forName("com.mysql.jdbc.Driver");
+            
+            // Cadena de conexión
             String bd = "videoclub";
             String login = "root";
             String password = "root";
             String url = "jdbc:mysql://192.168.33.10:3306/" + bd;
 
+            // Establecimiento de conexión
             Connection conexion = DriverManager.getConnection(url, login, password);
 
+            // [2] Consulta
             Statement sentencia = conexion.createStatement();
             ResultSet resultado = sentencia.executeQuery("select * from Peliculas");
 
             while (resultado.next() == true) {
                 System.out.println("Id: " + resultado.getInt("id"));
                 System.out.println("  Titulo: " + resultado.getString("titulo"));
-                System.out.println("  Año: " + resultado.getInt("anyo"));
+                System.out.println("  Año: " + resultado.getInt("anyo"));                
             }
 
+            // [3] Desconexión
             conexion.close();
 
         } catch (ClassNotFoundException ex) {
