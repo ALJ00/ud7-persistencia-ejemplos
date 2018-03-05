ff3bf8405e555c8804e35136f97eedb22bb3fd1c
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2018-03-05 09:33:01 +0100

    Gestión de dependencias con Maven

---
+++ 03_maven/PruebaMaven/src/main/java/com/jaureguialzo/Main.java
@@ -0,0 +1,16 @@
+package com.jaureguialzo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        logger.debug("Mensaje de depuración");
+        logger.error("Mensaje de error");
+
+    }
+}
